05b3bb5f80bc624e9ebd9a507aa36eb473aea978
Tiisetso Tjabane <t.tjabane@gmail.com>
2020-03-01 14:40:20 +0200

    first commit

---
+++ Solutions/InterviewPreparationKit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class InterviewPreparationKit
+    {
+        public int[] LeftRotation(int[] a, int d)
+        {
+            return new int[] { };
+        }
+
+        public int TwoDArrayDs(int[][] arr)
+        {
+            var sums = new List<int>();
+            for (var i = 0; i < arr.Length - 3; i++)
+            {
+                for(var j=0; j<arr[0].Length-3; j++)
+                {
+
+                }
+            }
+        }
+
+
+        private int GetHourArraySum(int row, int col, int[][] dataSet)
+        {
+            var sum = 0;
+            for (var i = 0; i < 3; i++)
+            {
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Solutions/Warmup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Warmup
+    {
+        public string MiniMaxSum(int[] arr)
+        {
+            var ordered = arr.Select(Convert.ToInt64).OrderByDescending(n => n);
+            var max = ordered.Take(4).Sum();
+            var min = ordered.Skip(1).Sum();
+            return $"{min} {max}";
+        }
+
+
+        public int CountingValleys(string path)
+        {
+            var numPath = path.Select(c => c == 'D' ? -1 : 1).ToList();
+            var trajectory = new List<int>();
+            for (var i = 0; i < numPath.Count; i++)
+            {
+                if (i == 0)
+                    trajectory.Add(numPath[i]);
+                else
+                    trajectory.Add(numPath[i] + trajectory.Last());
+            }
+
+            var valleys = trajectory.Select((point, index) => new { value = point, i = index })
+                                        .Count(point => point.value == 0 && trajectory[point.i - 1] < 0);
+            return valleys;
+        }
+
+
+        static int JumpingOnClouds(int[] path, int step, int currentLoc)
+        {
+            if (currentLoc > path.Length - 1)
+                return int.MaxValue;
+            if (path[currentLoc] == 0 && currentLoc == path.Length - 1)
+                return step;
+            if (path[currentLoc] == 1)
+                return int.MaxValue;
+
+            var next = step + 1;
+            Console.WriteLine($"step : {next}, currentLoc: {currentLoc}, currentValue: {path[currentLoc]}");
+            return Math.Min(JumpingOnClouds(path, next, currentLoc + 1),
+                JumpingOnClouds(path, next, currentLoc + 2));
+        }
+
+
+
+    }
+}
